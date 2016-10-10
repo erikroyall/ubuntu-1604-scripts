51edcb260921b651d2e5c07c4f25d2bd9d80b0a7
Erik Royall <erikroyall@hotmail.com>
2016-10-10 09:09:11 +0530

    Create ruby-ror.sh

---
+++ lang/ruby-ror.sh
@@ -0,0 +1,17 @@
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+
+rbenv install 2.3.1
+rbenv global 2.3.1
+ruby -v
+gem install bundler
+gem install rails -v 4.2.6
+rbenv rehash
+rails -v
