122939def3ad8b3ce5c171d4695a0abc0d8029b8
Erik Royall <erikroyall@hotmail.com>
2016-10-10 09:08:12 +0530

    Create node-nvm.sh

---
+++ lang/node-nvm.sh
@@ -0,0 +1,4 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install node
