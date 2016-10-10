92c949bace782752c90617b402279df0b0fca76d
Erik Royall <erikroyall@hotmail.com>
2016-10-10 09:09:38 +0530

    Create mysql-postgres.sh

---
+++ lang/mysql-postgres.sh
@@ -0,0 +1,10 @@
+# MySQL
+sudo apt-get install mysql-server mysql-client libmysqlclient-dev
+
+# PostgreSQL
+sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql-common
+sudo apt-get install postgresql-9.5 libpq-dev
+
